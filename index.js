7e656491e92c4dbdeab85b932b8589d551f0bafe
Aji Maulana <mancode700@gmail.com>
2024-02-17 20:16:27 +0700

    Add: Set cookie

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-import mysqlConnection from "./db-connection.js";
 import express from "express";
 import bodyParser from "body-parser";
 import bcrypt from "bcrypt";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import mysqlConnection from "./db-connection.js";
 
-let isInstance = false;
-let app;
-
+const app = express();
 const port = 3000;
-
-if (!isInstance) {
-  app = express();
-  isInstance = true;
-}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
+app.use(cookieParser());
 
-// Koneksi database Mysql
 mysqlConnection.connect((err) => {
   if (err) {
     console.error("Error connecting to database:", err);
@@ -27,61 +21,15 @@ mysqlConnection.connect((err) => {
   console.log("Connected to database");
 });
 
-app.get('/', (req, res) => res.status(200).send('Selamat datang di API api-blind-code'))
+app.get("/", (req, res) => {
+  res.status(200).send("Selamat datang di API api-blind-code");
+});
 
 // Registrasi
-app.post("/register", (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    // Hash password sebelum menyimpan ke database
-    bcrypt.hash(password, 10, function (err, hash) {
-      const query = "INSERT INTO users (username, password) VALUES (?, ?)";
-      mysqlConnection.query(
-        query,
-        [username, hash],
-        (err, result) => {
-          if (err) {
-            console.error("Error registering user:", err);
-            res.status(500).send("Error registering user");
-          } else {
-            res.status(201).send("User registered successfully");
-          }
-        }
-      );
-    });
-  } catch (error) {
-    console.error("Error hashing password:", error);
-    res.status(500).send("Error hashing password");
-  }
-});
+app.post("/register", registerUser);
 
 // Login
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-
-  const query = "SELECT * FROM users WHERE username = ?";
-  mysqlConnection.query(query, [username], async (err, results) => {
-    if (err) {
-      console.error("Error querying database:", err);
-      res.status(500).send("Error querying database");
-    } else if (results.length > 0) {
-      try {
-        const match = await bcrypt.compare(password, results[0].password);
-        if (match) {
-          res.status(200).send("Login successful");
-        } else {
-          res.status(401).send("Incorrect password");
-        }
-      } catch (error) {
-        console.error("Error comparing passwords:", error);
-        res.status(500).send("Error comparing passwords");
-      }
-    } else {
-      res.status(401).send("User not found");
-    }
-  });
-});
+app.post("/login", loginUser);
 
 // Menangani penutupan koneksi setelah server berhenti
 process.on("SIGINT", () => {
@@ -95,6 +43,63 @@ process.on("SIGINT", () => {
     process.exit(0);
   });
 });
+
+function registerUser(req, res) {
+  const { username, password } = req.body;
+
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      console.error("Error hashing password:", err);
+      res.status(500).send("Error hashing password");
+      return;
+    }
+
+    const query = "INSERT INTO users (username, password) VALUES (?, ?)";
+    mysqlConnection.query(query, [username, hash], (err, result) => {
+      if (err) {
+        console.error("Error registering user:", err);
+        res.status(500).send("Error registering user");
+        return;
+      }
+      res.status(201).send("User registered successfully");
+    });
+  });
+}
+
+async function loginUser(req, res) {
+  const { username, password } = req.body;
+
+  try {
+    const query = "SELECT * FROM users WHERE username = ?";
+    const results = await mysqlConnection.query(query, [username]);
+
+    if (results.length > 0) {
+      const match = await bcrypt.compare(password, results[0].password);
+      if (match) {
+        setLoginCookie(res, username);
+        res.status(200).send("Login successful");
+      } else {
+        res.status(401).send("Incorrect password");
+      }
+    } else {
+      res.status(401).send("User not found");
+    }
+  } catch (error) {
+    console.error("Error querying database or comparing passwords:", error);
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+function setLoginCookie(res, username) {
+  if (!res.cookies.key) {
+    res.cookie("key", username, {
+      maxAge: 604800000, // 7 hari dalam milidetik
+      expires: new Date(Date.now() + 604800000), // 7 hari dari sekarang
+      secure: true, // Hanya dapat diakses melalui HTTPS,
+      path: "/",
+    });
+  }
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
